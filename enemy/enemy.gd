2d0212fa5ab6ca774abf843d9ac0b27b0823fac7
Z-Graham <2260220@flourbluffschools.org>
2026-01-20 11:57:22 -0600

    base

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -2,5 +2,9 @@ extends PathFollow3D
 ##The speed in m/s the enemy will move
 @export var speed=5
 
+@onready var base = get_tree().get_first_node_in_group("Base")
 func _process(delta: float) -> void:
 	progress+=speed*delta
+	if progress_ratio==1:
+		base.take_damage()
+		queue_free()
