e6c795d096c6120a5f509516fe1d8c4ceed3a334
Z-Graham <2260220@flourbluffschools.org>
2026-01-20 12:32:40 -0600

    multiple enemies

--- Base/base.gd
+++ Base/base.gd
@@ -1,5 +1,16 @@
 extends Node3D
 class_name Base
 
+@onready var health_label: Label3D = $Label3D
+@export var max_health:=5
+var health: int:
+	set(num):
+		health=num
+		health_label.text=str(health)+"/"+ str(max_health)
+		if num==0:
+			get_tree().reload_current_scene()
+func _ready() -> void:
+	health=max_health
 func take_damage():
 	print("Damage taken")
+	health-=1
