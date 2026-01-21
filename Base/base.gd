a1d1f2afb7e866c42d3029e28e4035aea70d1882
Z-Graham <2260220@flourbluffschools.org>
2026-01-21 12:19:33 -0600

    health label

--- Base/base.gd
+++ Base/base.gd
@@ -7,11 +7,16 @@ var health: int:
 	set(num):
 		health=num
 		health_label.text=str(health)+"/"+ str(max_health)
+		var red=Color.RED
+		var white=Color.WHITE
+		var color=red.lerp(white,health*1.0/max_health)
+		health_label.modulate=color
+		
+		
 		if num==0:
 			get_tree().reload_current_scene()
 func _ready() -> void:
 	health=max_health
-	health_label.modulate=Color.RED
 func take_damage():
 	print("Damage taken")
 	health-=1
