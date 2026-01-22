12eb26f3d64be5016a3f84198f19b4f358caae82
Z-Graham <2260220@flourbluffschools.org>
2026-01-22 12:40:30 -0600

    turrets rotate

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
 var projectile=preload("res://Turret/projectile.tscn")
+var enemy_path:Path3D
 
+func _physics_process(delta: float) -> void:
+	var enemy= enemy_path.get_children().back()
+	look_at(enemy.global_position,Vector3.UP,true)
 func _on_timer_timeout() -> void:
+
 	var proj=projectile.instantiate()
 	add_child(proj)
 	proj.global_position=global_position
+	
