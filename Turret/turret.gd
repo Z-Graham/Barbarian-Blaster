e7257d15f6289c4ce9908d7ac1adbeaad32158d0
Z-Graham <2260220@flourbluffschools.org>
2026-01-22 12:26:45 -0600

    projectiles

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,1 +1,8 @@
 extends Node3D
+
+var projectile=preload("res://Turret/projectile.tscn")
+
+func _on_timer_timeout() -> void:
+	var proj=projectile.instantiate()
+	add_child(proj)
+	proj.global_position=global_position
